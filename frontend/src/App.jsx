a4ebf06ae744729e944780f6a4468a217d481f09
BAndris4 <andrisbalogh444@gmail.com>
2025-10-06 18:29:52 +0200

    chore: setup React Vite frontend with TailwindCSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-5xl">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
