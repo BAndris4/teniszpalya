90c49898de13d3d2335d0b104c13fa9d0e278e54
imIwKy <iwkymain@gmail.com>
2025-10-23 14:59:05 +0200

    feat: Added session management via JWT tokens. + Added two test users an admin and a normal one. + Added a /login endpoint + Added an enum for Roles

--- API/Teniszpalya.API/Program.cs
+++ API/Teniszpalya.API/Program.cs
@@ -1,5 +1,8 @@
 
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Teniszpalya.API.Data;
 
 namespace Teniszpalya.API;
@@ -27,6 +30,39 @@ public class Program
         builder.Services.AddDbContext<AppDBContext>(options =>
         options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
         builder.Services.AddControllers();
+
+        var key = Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]);
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(key)
+            };
+
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    if (context.Request.Cookies.ContainsKey("AuthToken"))
+                        context.Token = context.Request.Cookies["AuthToken"];
+                    return Task.CompletedTask;
+                }
+            };
+        });
+
+        builder.Services.AddAuthorization();
 
 
         var app = builder.Build();
